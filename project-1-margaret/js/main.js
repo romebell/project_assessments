e836a5f9b0160e844aa0ee01c15f644f84e124c8
margare-jihua <margaret.jihua@gmail.com>
2020-08-03 10:33:38 -0700

    red negative number

--- project-1-margaret/js/main.js
+++ project-1-margaret/js/main.js
@@ -6,12 +6,25 @@ let output = document.querySelector('h1')
 
 let defaultNum = parseInt(input.defaultValue)
 
+function negative (num) {
+    if (num < 0){
+        return true
+    }
+}
+
 plus.addEventListener('click', function() {
     let currentNum = parseInt(output.textContent)
     let inputNum = parseInt(input.value)
 
     currentNum += inputNum
     output.textContent = currentNum
+
+    if (currentNum < 0) {
+        output.style.color = 'red'
+    }
+    else {
+        output.style = ''
+    }    
 })
 
 minus.addEventListener('click', function() {
@@ -20,5 +33,12 @@ minus.addEventListener('click', function() {
 
     currentNum -= inputNum
     output.textContent = currentNum
+
+    if (currentNum < 0) {
+        output.style.color = 'red'
+    }
+    else {
+        output.style = ''
+    }
 })
 
