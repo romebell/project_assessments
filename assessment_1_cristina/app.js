3c46435545a0d76f4c67791cbe8601215b0e803a
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-08-03 11:08:09 -0700

    minus button working

--- assessment_1_cristina/app.js
+++ assessment_1_cristina/app.js
@@ -1,5 +1,4 @@
-console.log("this works");
-
+//console.log("this works");
 let plus = document.querySelector(".plus");
 let minus = document.querySelector(".minus");
 let result = document.querySelector(".counter");
@@ -7,18 +6,26 @@ let input = document.querySelector(".input");
 
 document.addEventListener("DOMContentLoaded", function(){
 //when + is pressed take value from input and add it to result score
-
 function add() {
     let currentNum = Number(result.textContent);
     let inputNum = Number(input.value);
     currentNum += inputNum;
     result.textContent = currentNum;
-}
+    if (currentNum < 0) {
+        result.style.color = "red";
+} }
 
 plus.addEventListener("click",add);
 
 function subtract() {
-    
+    let currentNum = Number(result.textContent);
+    let inputNum = Number(input.value);
+    currentNum -= inputNum;
+    result.textContent = currentNum;
+    if (currentNum < 0) {
+        result.style.color = "red";
+    }
+
 }
 minus.addEventListener("click",subtract);
 
