260945430d54f35571cc883044e3cc700cd03c27
Martin Briceno <martin.briceno@live.com>
2020-08-03 14:01:53 -0400

    plus and minus fully functional with red numbers too

--- project-1-martinbriceno/js/app.js
+++ project-1-martinbriceno/js/app.js
@@ -1,6 +1,6 @@
+let plus = document.getElementById('plus');
+let minus = document.getElementById('minus');
 let result = document.getElementById('result').textContent;
-// let plus = document.getElementById('plus');
-// let minus = document.getElementById('minus');
 let inputNumb = document.getElementById('inputNumb').value;
 let numb1 = parseInt(result);
 let numb2 = parseInt(inputNumb);
@@ -15,19 +15,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     function plusNumber () {
+        let result = document.getElementById('result').textContent;
+        let inputNumb = document.getElementById('inputNumb').value;
+        let numb1 = parseInt(result);
+        let numb2 = parseInt(inputNumb);
         let numb3 = numb1 + numb2;
-        document.getElementById('result').innerHTML = numb3;
-        console.log(numb3);
-        result = numb3;
+        if (numb3 < 0) {
+            document.getElementById('result').style.color = 'red';
+            document.getElementById('result').innerHTML = numb3;
+
+        } else {
+            document.getElementById('result').style.color = 'black';
+            document.getElementById('result').innerHTML = numb3;
+        }
 
     };
 
     function minusNumber () {
 
+        let result = document.getElementById('result').textContent;
+        let inputNumb = document.getElementById('inputNumb').value;
+        let numb1 = parseInt(result);
+        let numb2 = parseInt(inputNumb);
         let numb3 = numb1 - numb2;
-        document.getElementById('result').innerHTML = numb3;
-        console.log(result);
-        result = numb3;
+        if (numb3 < 0) {
+            document.getElementById('result').style.color = 'red';
+            document.getElementById('result').innerHTML = numb3;
+
+        } else {
+            document.getElementById('result').style.color = 'black';
+            document.getElementById('result').innerHTML = numb3;
+        }
+
+
+
 
     };
 });
