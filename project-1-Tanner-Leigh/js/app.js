a015000d525bd9ee91ca95a4447329e54d57024a
Tanner Leigh <tannerhleigh@gmail.com>
2020-08-03 09:59:39 -0700

    [javascript] add working javascript DOM manipulation

--- project-1-Tanner-Leigh/js/app.js
+++ project-1-Tanner-Leigh/js/app.js
@@ -1,16 +1,24 @@
 console.log("hello front end")
 let result = 0
 
-
 function add() {
     result += input;
+    console.log(result);
+    changeNumber();
 }
 document.getElementById('plus').addEventListener('click', add);
 
+function subtract() {
+    result -= input;
+    console.log(result);
+    changeNumber();
+}
+document.getElementById('minus').addEventListener('click', subtract);
+
 function changeNumber() {
-    let number = document.getElementById('number');
+    document.getElementById('number').innerHTML=result;
 }
 
 
-let input = document.getElementById('input').defaultValue="1"
+let input = document.getElementById('input').defaultValue=1
 console.log(input)
