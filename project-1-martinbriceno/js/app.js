87e2dbce4aea7b9ad382a999e70935c35c0953af
Martin Briceno <martin.briceno@live.com>
2020-08-03 13:51:55 -0400

    adds only once

--- project-1-martinbriceno/js/app.js
+++ project-1-martinbriceno/js/app.js
@@ -1,5 +1,33 @@
-let result = document.getElementById('result').value;
-let plus = document.getElementById('plus');
-let
+let result = document.getElementById('result').textContent;
+// let plus = document.getElementById('plus');
+// let minus = document.getElementById('minus');
+let inputNumb = document.getElementById('inputNumb').value;
+let numb1 = parseInt(result);
+let numb2 = parseInt(inputNumb);
 
-document.addEventListener('DOMContentLoaded', () => { }
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.getElementById('plus').addEventListener('click', plusNumber);
+    document.getElementById('minus').addEventListener('click', minusNumber);
+
+
+    function plusNumber () {
+        let numb3 = numb1 + numb2;
+        document.getElementById('result').innerHTML = numb3;
+        console.log(numb3);
+        result = numb3;
+
+    };
+
+    function minusNumber () {
+
+        let numb3 = numb1 - numb2;
+        document.getElementById('result').innerHTML = numb3;
+        console.log(result);
+        result = numb3;
+
+    };
+});
