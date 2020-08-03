be23fdbaf24abadf35d83843fb65b85200eaf6fc
alpha-martinez <martinezalphaa@gmail.com>
2020-08-03 14:08:56 -0400

    adding functions to add and subtract functions to  js

--- app.js
+++ app.js
@@ -2,10 +2,36 @@ document.addEventListener('DOMContentLoaded', function (){
 
 
  //call your DOM elements here
-
  const plusBtn = document.getElementById('plus-btn');
  const minusBtn = document.getElementById('minus-btn');
+ let inputField = document.getElementById('input-field');
+ let displayTotal = document.getElementById('display');
+ 
+var count = 0;
+//functions here
+
+let showNum = () => {
+    displayTotal.textContent = count;
+}
 
 
+ const addInput = () => {
+    inputField = parseInt(inputField.value);
+    count = count + inputField;
+    console.log(count);
+    showNum();
+ }
+
+
+const subtractInput = () => {
+    inputField = parseInt(inputField.value);
+    count = count - inputField;
+    console.log(count);
+    showNum();
+}
+
+//event listeners for on click
+plusBtn.addEventListener('click', addInput);
+minusBtn.addEventListener('click', subtractInput);
 
 })
