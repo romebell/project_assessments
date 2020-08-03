db6fb53b57bf5d336c95ff425cba11e29c979539
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-08-03 11:03:20 -0700

    add js working

--- assessment_1_cristina/app.js
+++ assessment_1_cristina/app.js
@@ -6,6 +6,20 @@ let result = document.querySelector(".counter");
 let input = document.querySelector(".input");
 
 document.addEventListener("DOMContentLoaded", function(){
+//when + is pressed take value from input and add it to result score
 
+function add() {
+    let currentNum = Number(result.textContent);
+    let inputNum = Number(input.value);
+    currentNum += inputNum;
+    result.textContent = currentNum;
+}
+
+plus.addEventListener("click",add);
+
+function subtract() {
+    
+}
+minus.addEventListener("click",subtract);
 
 });
