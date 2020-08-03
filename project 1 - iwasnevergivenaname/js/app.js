17ff5429170b6db65b92fe49c4b0538571e2fcc2
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-08-03 10:29:52 -0700

    remove console.log checks

--- project 1 - iwasnevergivenaname/js/app.js
+++ project 1 - iwasnevergivenaname/js/app.js
@@ -8,32 +8,22 @@ output.textContent = outputVal;
 
 
 minus.addEventListener("click", function () {
-    console.log("clicked minus");
     const input = document.getElementById("int").value;
-    console.log(`input is ${input}`);
     outputVal -= input;
-    console.log(`outputVal is ${outputVal}`);
     if (outputVal < 0) {
-        console.log("this shit is getting negative");
         output.style.color = "red";
     } else {
-        console.log("we're trying to stay positive");
         output.style.color = "black";
     }
     output.textContent = outputVal;
 });
 
 plus.addEventListener("click", function () {
-    console.log("clicked plus");
     const input = document.getElementById("int").value;
-    console.log(`input is ${input}`);
     outputVal += Number(input);
-    console.log(`outputVal is ${outputVal}`);
     if (outputVal < 0) {
-        console.log("this shit is getting negative");
         output.style.color = "red";
     } else {
-        console.log("we're trying to stay positive");
         output.style.color = "black";
     }
     output.textContent = outputVal
