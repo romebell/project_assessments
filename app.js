6824738414cbd277956f359f6617c36709242ae1
Nicholas Phillips <nicholasphillips@Nicholass-MacBook-Air.local>
2020-08-03 12:15:18 -0700

    assessment

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const buttonInc = document.getElementById('increase');
+const buttonDec = document.getElementById('decrease');
+const numValue = document.getElementById("input").value
+function numbers() {
+ increase.addEventListener('click', addNumber) 
+ decrease.addEventListener('click', subNumber)
+}
+
+
+const addNumber = e => {
+for(let i = 0; i < numValue.length; i++){
+    console.log(numValue)
+}
+    
+}
+console.log(increase)
+
+
+console.log(increase)
+const subNumber = e => {
+    for(let i = 0; i < numValue.length; i--){
+        console.log(numValue)
+
+}
+
+}
