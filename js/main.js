2b01cf3cc2fad79f75ddb67891025b212a99b446
Elaine <elaine@Felipes-iMac.lan1>
2020-08-03 11:38:14 -0500

    [js] add elements and check identities

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let negative = document.getElementById("negative");
+let positive = document.getElementById("positive");
+let input = document.getElementById("input");
+let value = document.getElementById("value");
+
+negative.addEventListener("click", function(){
+    console.log("hello")
+})
+positive.addEventListener("click",  function(){
+    console.log("goodbye")
+})
