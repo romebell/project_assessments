b26069210656d1b57b97f159c216267d507eb1c5
Lizz <lizwesterband@gmail.com>
2020-08-03 12:28:02 -0700

    [final] Add final attempt; parseInt non functional

--- project-1-Lizzwest/js/main.js
+++ project-1-Lizzwest/js/main.js
@@ -1,27 +1,31 @@
 console.log('My files are connected');
 
+
+// I know i need to include parseInt, but each place I try and implement 
+//it throws back NaN in my output
 document.addEventListener('DOMContentLoaded', function() {
 	let input = (document.getElementById('input').defaultValue = 1);
     
     let finalNumber = 0;
-
 	function plusClick() {
-		finalNumber += input;
+		//finalNumber += parseInt(input.value);
+		finalNumber += input
 		
 		newNumber();
 	}
 	document.getElementById('plus').addEventListener('click', plusClick);
-
 	function minusClick() {
-        finalNumber -= input;
-		
-        newNumber();
+       // finalNumber -= parseInt(input.value);
+		finalNumber-= input
+		newNumber();
         
+
 	}
 	document.getElementById('minus').addEventListener('click', minusClick);
 
 	function newNumber() {
 		document.getElementById('output').innerHTML = finalNumber;
 	}
-    
+	
+	
 });
