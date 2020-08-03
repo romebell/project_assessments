bebbe32ee9d720a12f1a461ede0a0fae450e52c4
alpha-martinez <martinezalphaa@gmail.com>
2020-08-03 13:06:55 -0400

    adding hover to the buttons

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function (){
 
 
+ //call your DOM elements here
 
-    
+ const plusBtn = document.getElementById('plus-btn');
+ const minusBtn = document.getElementById('minus-btn');
+
+
+
 })
