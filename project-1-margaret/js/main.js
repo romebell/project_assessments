1df2c8e6f5ecbeb118a8cb7a70c854cac81c406c
margare-jihua <margaret.jihua@gmail.com>
2020-08-03 09:50:47 -0700

    css layout

--- project-1-margaret/js/main.js
+++ project-1-margaret/js/main.js
@@ -1,1 +1,15 @@
 // console.log('hello');
+let plus = document.querySelector('.plus')
+let minus = document.querySelector('.minus')
+let input = document.querySelector('input')
+let output = document.querySelector('h1')
+
+plus.addEventListener('click', function() {
+    console.log(input.value);
+})
+
+minus.addEventListener('click', function() {
+    console.log(input.value);
+})
+
+console.log(output.textContent+1);
