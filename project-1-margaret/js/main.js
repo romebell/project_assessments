ce2ae68d52eaa713abd887e36a8ed58666bfd732
margare-jihua <margaret.jihua@gmail.com>
2020-08-03 10:02:11 -0700

    plus button

--- project-1-margaret/js/main.js
+++ project-1-margaret/js/main.js
@@ -4,12 +4,17 @@ let minus = document.querySelector('.minus')
 let input = document.querySelector('input')
 let output = document.querySelector('h1')
 
+let currentNum = parseInt(output.textContent)
+let inputNum = parseInt(input.value)
+
 plus.addEventListener('click', function() {
-    console.log(input.value);
+    currentNum += inputNum
+    
+    output.textContent = currentNum
 })
 
 minus.addEventListener('click', function() {
     console.log(input.value);
 })
 
-console.log(output.textContent+1);
+console.log(typeof currentNum, typeof inputNum);
