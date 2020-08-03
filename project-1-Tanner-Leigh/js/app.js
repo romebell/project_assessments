d579882cf9e46ac8dea86336a7b7380586e8a403
Tanner Leigh <tannerhleigh@gmail.com>
2020-08-03 11:11:27 -0700

    [javascript] finish javascript

--- project-1-Tanner-Leigh/js/app.js
+++ project-1-Tanner-Leigh/js/app.js
@@ -1,24 +1,31 @@
 console.log("hello front end")
 let result = 0
 
-function add() {
-    result += input;
+function add(e) {
+    result += parseInt(input.value);
     console.log(result);
+    e.preventDefault();
     changeNumber();
 }
 document.getElementById('plus').addEventListener('click', add);
 
-function subtract() {
-    result -= input;
+function subtract(e) {
+    result -= parseInt(input.value);
     console.log(result);
+    e.preventDefault();
     changeNumber();
 }
 document.getElementById('minus').addEventListener('click', subtract);
 
 function changeNumber() {
     document.getElementById('number').innerHTML=result;
+    if (result < 0 ) {
+        document.getElementById('number').style.color = 'red'
+    } else {
+        document.getElementById('number').style.color = 'black'
+    }
 }
 
-
-let input = document.getElementById('input').defaultValue=1
-console.log(input)
+let input = document.getElementById('input');
+input.defaultValue=1;
+console.log(input);
