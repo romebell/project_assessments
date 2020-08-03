4aed63acec0ee80f00e5766f3ae47a14dd61f0e8
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-08-03 09:55:43 -0700

    [app.js] Add global variables and set intial values

---
+++ project_1_cabassa/app.js
@@ -0,0 +1,8 @@
+const modifier = document.getElementById("modifier");
+const display = document.getElementById("display");
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+
+display.innerText = "0";
+modifier.value = "1";
+
