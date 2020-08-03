50e20d2fc974484e90cf5a45311f67b98582786c
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-08-03 12:33:57 -0700

    [Assessment]

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const inputNum = document.querySelector('.input-number');
+const plusButton = document.querySelector('.increase-btn');
+const minusButton = document.querySelector('.decrease-btn');
+const mainOuput = document.querySelector('.main-output')
+let count = 0;
+
+plusButton.addEventListener('click',function() {
+        
+        mainOuput.textContent = count ++
+        if (inputNum.value === )
+
+    
+});
+minusButton.addEventListener('click',function() {
+    mainOuput.textContent = count--
+});
