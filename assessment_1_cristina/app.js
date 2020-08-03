9302aa151510a4db2cc8d07a4f823c4ff2ed9457
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-08-03 09:53:40 -0700

    add DOM listener and fix css

--- assessment_1_cristina/app.js
+++ assessment_1_cristina/app.js
@@ -1,1 +1,11 @@
 console.log("this works");
+
+let plus = document.querySelector(".plus");
+let minus = document.querySelector(".minus");
+let result = document.querySelector(".counter");
+let input = document.querySelector(".input");
+
+document.addEventListener("DOMContentLoaded", function(){
+
+
+});
