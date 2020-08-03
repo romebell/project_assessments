a6867716454c7a383f1a517c9936748ff625dd7e
Lizz <lizwesterband@gmail.com>
2020-08-03 11:20:24 -0700

    [js] Add ability to +/- by one

--- project-1-Lizzwest/js/main.js
+++ project-1-Lizzwest/js/main.js
@@ -1,9 +1,24 @@
-console.log("My files are connected")
+console.log('My files are connected');
 
 document.addEventListener('DOMContentLoaded', function() {
-let finalNumber = 0
+	let finalNumber = 0;
 
+	function plusClick() {
+		finalNumber += input;
+		
+		newNumber();
+	}
+	document.getElementById('plus').addEventListener('click', plusClick);
 
+	function minusClick() {
+		finalNumber -= input;
+		
+		newNumber();
+	}
+	document.getElementById('minus').addEventListener('click', minusClick);
 
-
-})
+	function newNumber() {
+		document.getElementById('output').innerHTML = finalNumber;
+	}
+	let input = (document.getElementById('input').defaultValue = 1);
+});
