ecaed362451ec1a8487179e0483035b4c15b9854
mac <mac@macs-MacBook-Air.local>
2020-08-03 13:32:06 -0700

    [js] closer to expected result

--- js/main.js
+++ js/main.js
@@ -2,45 +2,65 @@
 
 
 
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
     let output = document.getElementById('total');
-    let number = document.getElementById('input');
+    let numbers = document.getElementById('number');
     let add = document.getElementById('adds');
     let sub = document.getElementById('subs');
     // output.innerText = `${result}`
+   
+
+
+    output = 0;
+    // number = 1;
+    let result;
+
+    // function currentNum(){
+    //     output.textContent = output;
+    // }
+
+    function one(){
+        let numChange = parseInt(numbers.value);
+        let originalNum = parseInt(output.textContent);
+
+        result = numChange + originalNum;
+        // console.log(result);
+        output.textContent = result;
+    }
+
+    function two(){
+        let numChange = parseInt(numbers.value);
+        let originalNum = parseInt(output.textContent);
+
+        result = numChange - originalNum;
+        // console.log(result);
+        output.textContent = result;
+    }
+    // output.textContent = result;
+    // console.log(output);
+        // console.log('clicked');
+        // let numbers = number.value;
+        // console.log(numbers);
+        // result = numbers += output;
+        // console.log(result);
+        // output.textContent = `${result}`;
+        // return result;
+        // currentNum();
+        
+    
+    // output.textContent = `${result}`;
+    // console.log(result);
+   
+
+    // function two(number, output){
+    //     console.log('clicked');
+    //    result = number-=output;
+    //    output.textContent = `${result}`;
+    // }
+    
+    // output.innerText = `${result}`
+
     add.addEventListener('click', one);
     sub.addEventListener('click', two);
 
-
-    // output = 0;
-    number = 1;
-    let result;
-
-    function currentNum(){
-        output.textContent = output;
-    }
-
-
-    function one(number, output){
-        console.log('clicked');
-        let numbers = number(document.getElementById(input).value);
-        result = numbers += output;
-        // console.log(result);
-        // output.textContent = `${result}`;
-        return result;
-        currentNum();
-        
-    }
-    // output.textContent = `${result}`;
-    console.log(result);
-   
-
-    function two(number, output){
-        console.log('clicked');
-       result = number-=output;
-       output.textContent = `${result}`;
-    }
-    
-    output.innerText = `${result}`
-
-})
+// })
