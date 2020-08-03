f77ca0f581086f5de4ea51e2149f30cc2d517e07
Elaine <elaine@Felipes-iMac.lan1>
2020-08-03 12:08:27 -0500

    [javascript] returns mvp function

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ let input = document.getElementById("input");
 let value = document.getElementById("value");
 
 negative.addEventListener("click", function(){
-    console.log((1*value.textContent)-(1*input.value));
+    value.textContent =(1*value.textContent)-(1*input.value) ;
 })
 positive.addEventListener("click",  function(){
-    console.log( (1*value.textContent)+(1*input.value) );
+    value.textContent= (1*value.textContent)+(1*input.value) ;
 })
