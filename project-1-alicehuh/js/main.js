ec237c5b281042a3b4df5434fb337f99b9d43aee
Alice Aekyung Huh <aklee213@gmail.com>
2020-08-03 21:43:37 -0400

    [Assessment] Resubmit

--- project-1-alicehuh/js/main.js
+++ project-1-alicehuh/js/main.js
@@ -1,75 +1,61 @@
 document.addEventListener('DOMContentLoaded', ()=> {
 
-const container = document.querySelector('.container')
-const element = document.querySelector('.element')
-
 const plusButton = document.querySelector('#plus-button')
 const minusButton = document.querySelector('#minus-button')
-currentValue = document.querySelector('.calculator').value 
-// const inputNumber = document.querySelector('input-number')
-
-
-
-
-
+const screen = document.querySelector('.calculator')
 
 plusButton.addEventListener('click', addNumber)
-function addNumber(){
-    inputNumber = document.querySelector('#input-number').value
-    console.log(inputNumber)
-    
-    screen = inputNumber + screen
-    displayNumber = document.querySelector('.calculator').value
-    console.log(displayNumber)
+plusButton.addEventListener('mouseover',changeColor)
+plusButton.addEventListener('mouseout', colorBack)
+minusButton.addEventListener('click', minusNumber)
+minusButton.addEventListener("mouseover", changeColor)
+minusButton.addEventListener('mouseout', colorBack)
 
+document.querySelector('#input-number').value = 1
+document.querySelector('.calculator').innerHTML = 0
+
+function addNumber(){
+
+    inputNumber = document.querySelector('#input-number').value   
+    displayNumber = document.querySelector('.calculator').innerHTML
+    results = Number(inputNumber)+Number(displayNumber) 
+    screen.innerText = results
+
+    if(results<0){
+        screen.style.color = "red";  
+    }else{
+        screen.style.color = "black";
+    }
 }
 
 
 
-// display.setAttribute('id', 'display')
-// const plusButton = document.createElement('button')
-// plusButton.setAttribute('id','plus-button')
-// plusButton.innerText = '+'
-// const minusButton = document.createElement('button')
-// minusButton.setAttribute('id','minus-button')
-// minusButton.innerText = '-'
-// const inputNumber = document.createElement('input')
-// function dsiplayNumber(){
+function minusNumber(){
+    inputNumber = document.querySelector('#input-number').value
+    displayNumber = document.querySelector('.calculator').innerHTML
+    results = Number(displayNumber)-Number(inputNumber)
+    screen.innerText = results
     
-
-
-// }
-
-
-// var x = document.getElementById("input-number").value;
-
-
-
-// plusButton.addEventListener('click',addNumber(inputX,displayedNum))
-
-// function addNumber(x,y){
-//     var inputX = document.getElementById("input-number").value;
-//     var y = inputX + 
-
-// }
-
-
-
-
-
-// function addNumber(x,y){
+    if(results<0){
+        screen.style.color = "red";  
+    }else{
+        screen.style.color = "black";
+    }
     
-//    resultNumber.innerText = x+y
+    
+}
 
-// //     sum = inputX + displayedNum;
-// // console.log(sum)
+function changeColor(event){
+    event.target.style.backgroundColor = "white";
+    event.target.style.color = "rgb(189, 188, 188)";    
+}
+
+function colorBack(event){
+    event.target.style.backgroundColor = "rgb(189, 188, 188)";
+    event.target.style.color = "white";
+}
 
 
-// }
-// var inputX = document.getElementById("input-number").value;
-// var displayedNum = resultNumber.innerHTML;
-// console.log(inputX)
-// console.log(displayedNum)
 
 
 })
