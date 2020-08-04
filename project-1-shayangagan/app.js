359dc50ca41f7a81dc48ad3f1b2f0397d4467901
Shayan Gagan <shayangagan@Shayans-MacBook-Pro.local>
2020-08-04 00:48:23 -0500

    [modified counter] refactored my code to satisfy the requirements set by the readme.

--- project-1-shayangagan/app.js
+++ project-1-shayangagan/app.js
@@ -1,11 +1,15 @@
 let btnAdd = document.querySelector('#add');
 let btnSubtract = document.querySelector('#subtract');
 let input = document.querySelector('input');
+let display = document.querySelector('#display');
+display.value = 0
 
 btnAdd.addEventListener('click', () =>{
-    input.value = parseInt(input.value) + 1;
+    display.value = parseInt(display.value) + parseInt(input.value);
+    display.innerText = display.value;
 });
 
 btnSubtract.addEventListener('click', () =>{
-    input.value = parseInt(input.value) - 1;
+    display.value = parseInt(display.value) - parseInt(input.value);
+    display.innerText = display.value;
 });
